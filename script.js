7b27d09c1421ac2106ddc278a1fb8ef7b9856965
Sh3tss <gabrielggartist@gmail.com>
2025-09-02 21:01:18 -0300

    did the merge sorte part

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function fibonacci (n) {
 }
 console.log(fibonacci(8));
 
+function mergeSort(array){
+    if(array.length <=1){
+        return array;
+    }
+    const middle = Math.floor(array.length / 2);
+    leftHalf = array.slice(0, middle);
+    rightHalf = array.slice(middle);
+
+    const sortedLeft = mergeSort(leftHalf);
+    const sortedRight = mergeSort(rightHalf);
+
+    return merge(sortedLeft,sortedRight);
+}
+function merge(left, right){
+    const result = [];
+
+    while (left.length && right.length){
+        if (left[0] < right[0]){
+            result.push(left[0]);
+            left.shift();
+        }else {
+            result.push(right[0]);
+            right.shift();
+        }
+        
+    }
+    return result.concat(left,right);
+}
+console.log(mergeSort());
